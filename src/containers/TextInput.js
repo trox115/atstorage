fab583df2d05d72488f8b4145610916e1a2c5347
trox115 <tofernandes67@gmail.com>
2020-05-10 17:03:08 +0100

    fix indentations problems

--- src/containers/TextInput.js
+++ src/containers/TextInput.js
@@ -3,35 +3,35 @@ import { InputForm } from '../style';
 import PropTypes from 'prop-types';
 
 const TextInput = ({
-	name,
-	label,
-	onChange,
-	placeholder,
-	value,
-	error,
-	type,
+  name,
+  label,
+  onChange,
+  placeholder,
+  value,
+  error,
+  type,
 }) => {
-	return (
-		<InputForm
-			name={name}
-			label={label}
-			placeholder={placeholder}
-			value={value}
-			type={type}
-			onChange={onChange}
-			error={error}
-		/>
-	);
+  return (
+    <InputForm
+      name={name}
+      label={label}
+      placeholder={placeholder}
+      value={value}
+      type={type}
+      onChange={onChange}
+      error={error}
+    />
+  );
 };
 
 TextInput.propTypes = {
-	name: PropTypes.string.isRequired,
-	label: PropTypes.string.isRequired,
-	onChange: PropTypes.func.isRequired,
-	placeholder: PropTypes.string,
-	value: PropTypes.string,
-	error: PropTypes.string,
-	type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
 };
 
 export default TextInput;
